143af2ba3730ca6c66f4fc304ee81c439aa21291
Aprog96 <aarontrunick@gmail.com>
2021-10-27 20:36:39 -0400

    first commit

---
+++ Hands on 2/project01-02_txt.js
@@ -0,0 +1,20 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-2
+
+    Author: Aaron Trunick
+    Date:   
+
+    Filename: project01-02.js
+*/
+
+//define variables for service name and service speed
+
+var service1Name = 'Basic'; 
+var service2name = 'Express';
+var service3name = 'Extreme';
+var service4name = 'Ultimate';
+var service1speed = '0 Mbs';
+var service2speed = '100 Mbps';
+var service3speed = '500 Mbs';
+var service4speed = '1 Gig';
